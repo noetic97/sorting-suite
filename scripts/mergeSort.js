e0062738d1f5c98f5cdd7e4bdf775d618feb1f06
Joe Heitkamp <noetic97@gmail.com>
2017-05-18 11:12:52 -0600

    minor changes on merge

--- scripts/mergeSort.js
+++ scripts/mergeSort.js
@@ -6,20 +6,19 @@ const merge = (left, right) => {
       r = 0;
 
   while(l < lLen && r < rLen){
-     left[l] < right[r] ? result.push(left[l++]) : result.push(right[r++]);
+     left[l] < right[r] ? result.push(left[ l++ ]) : result.push(right[ r++ ]);
   }
   return result.concat(left.slice(l)).concat(right.slice(r));
 }
 
 const mergeSort = (arr) => {
-   let len = arr.length;
-   if(len <2)
+   if (arr.length < 2)
       return arr;
-   var mid = Math.floor(len/2),
-       left = arr.slice(0,mid),
+   var mid = Math.floor(arr.length / 2),
+       left = arr.slice(0, mid),
        right =arr.slice(mid);
 
-   return merge(mergeSort(left),mergeSort(right));
+   return merge(mergeSort(left), mergeSort(right));
 }
 
 export default mergeSort;
